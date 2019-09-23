7b74fb8ddf7310b659de582af30905122f8ec560
Suhyeon Lee <suhyeon917917@gmail.com>
2019-09-23 09:40:44 +0900

    #3 코드 구현 by 3512 이수현

--- FontandColor2/Form1.Designer.cs
+++ FontandColor2/Form1.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.RichTextBox rtbText;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.colorDlg = new System.Windows.Forms.ColorDialog();
             this.fontDlg = new System.Windows.Forms.FontDialog();
             this.tlsMenu = new System.Windows.Forms.ToolStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            rtbText = new System.Windows.Forms.RichTextBox();
+            this.tsbtnColor = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnFont = new System.Windows.Forms.ToolStripButton();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
             this.statusStrip1.SuspendLayout();
+            this.tlsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -49,35 +51,58 @@
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // tlsMenu
-            // 
-            this.tlsMenu.Location = new System.Drawing.Point(0, 0);
-            this.tlsMenu.Name = "tlsMenu";
-            this.tlsMenu.Size = new System.Drawing.Size(287, 25);
-            this.tlsMenu.TabIndex = 2;
-            this.tlsMenu.Text = "toolStrip1";
-            // 
-            // rtbText
-            // 
-            rtbText.Dock = System.Windows.Forms.DockStyle.Fill;
-            rtbText.Location = new System.Drawing.Point(0, 25);
-            rtbText.Name = "rtbText";
-            rtbText.Size = new System.Drawing.Size(287, 191);
-            rtbText.TabIndex = 3;
-            rtbText.Text = resources.GetString("rtbText.Text");
-            // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(136, 17);
             this.toolStripStatusLabel1.Text = "3512 이수현 - 폰트변경";
             // 
+            // tlsMenu
+            // 
+            this.tlsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnColor,
+            this.tsbtnFont});
+            this.tlsMenu.Location = new System.Drawing.Point(0, 0);
+            this.tlsMenu.Name = "tlsMenu";
+            this.tlsMenu.Size = new System.Drawing.Size(287, 25);
+            this.tlsMenu.TabIndex = 2;
+            this.tlsMenu.Text = "toolStrip1";
+            // 
+            // tsbtnColor
+            // 
+            this.tsbtnColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnColor.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnColor.Image")));
+            this.tsbtnColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnColor.Name = "tsbtnColor";
+            this.tsbtnColor.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnColor.Text = "toolStripButton1";
+            this.tsbtnColor.Click += new System.EventHandler(this.TsbtnColor_Click);
+            // 
+            // tsbtnFont
+            // 
+            this.tsbtnFont.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnFont.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnFont.Image")));
+            this.tsbtnFont.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnFont.Name = "tsbtnFont";
+            this.tsbtnFont.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnFont.Text = "toolStripButton2";
+            this.tsbtnFont.Click += new System.EventHandler(this.TsbtnFont_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbText.Location = new System.Drawing.Point(0, 25);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(287, 191);
+            this.rtbText.TabIndex = 3;
+            this.rtbText.Text = resources.GetString("rtbText.Text");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(287, 238);
-            this.Controls.Add(rtbText);
+            this.Controls.Add(this.rtbText);
             this.Controls.Add(this.tlsMenu);
             this.Controls.Add(this.statusStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -86,6 +111,8 @@
             this.Text = "3512 이수현";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.tlsMenu.ResumeLayout(false);
+            this.tlsMenu.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,6 +125,9 @@
         private System.Windows.Forms.FontDialog fontDlg;
         private System.Windows.Forms.ToolStrip tlsMenu;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripButton tsbtnColor;
+        private System.Windows.Forms.ToolStripButton tsbtnFont;
+        private System.Windows.Forms.RichTextBox rtbText;
     }
 }
 
